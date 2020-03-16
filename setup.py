8c885a30118f55f021ef115b30829af4c2ebbc97
Manuel Solimano <masolimano@uc.cl>
2020-03-16 18:47:58 -0300

    Wheel uploaded

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(name='visilens',
-    version='master',
+    version='0.1',
     description='Module for modeling gravitational lensing systems in which the data are not images but interferometric visibilities.',
     classifiers=[
     'Development Status :: 7 - Inactive',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 2.7',
     'Topic :: Scientific/Engineering :: Astronomy',
-    ]
+    ],
     keywords='visilens interferometry alma casa mc mc radioastronomy',
     url='http://github.com/masolimano/visilens',
     author='jspilker',
